19d62066ebe56b54f9d9de075beff8a2d7aceec0
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-31 04:15:16 +0530

    enabled cors

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 require('./database'); // database initialization
 
 var indexRouter = require('./routes/index');
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
