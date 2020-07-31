ca16f81f44b8f1098a3fb2a22a0d3a50d047e720
Pranav Acharya <apranav.acharya@gmail.com>
2020-08-01 05:03:54 +0530

    added api for record event and analytics report

---
+++ Server/routes/analytics.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../database');
+
+router.get('/recordClick/:id', function (req, res, next) {
+  db.run(
+    `UPDATE ads 
+    SET impression = impression + 1, bill = bill + CPI
+    WHERE id = ?`,
+    [req.params.id],
+    (err) => {
+      if (err) {
+        console.log('failed to capture event : ' + err.message);
+        res.status(400).json({ error: err.message });
+      } else {
+        res.status(200).json({ message: 'event captured' });
+      }
+    }
+  );
+});
+
+router.get('/report/:ssp', function (req, res, next) {
+  db.all(
+    `SELECT * FROM ads WHERE vendor = ?`,
+    [req.params.ssp],
+    (err, rows) => {
+      if (err) {
+        console.log('failed to generate report :' + err.message);
+        res.status(400).json({ error: err.message });
+      } else {
+        res.status(200).json({ ads: rows });
+      }
+    }
+  );
+});
+
+module.exports = router;
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/analytics', require('./analytics'));
 router.use('/ssp1', require('./ssp1'));
 router.use('/ssp2', require('./ssp2'));
 
